3fab974c2236c0a2073652507c8b10d35f96aff8
mennassaber <mennasaber202620@gmail.com>
2024-06-12 13:51:33 -0700

    shell

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,9 +1,2 @@
 #!/usr/bin/env ruby
-
-input = ARGV[0]
-
-# Regular expression to match 'aaa' as a sequence
-regex = /a{3}/
-
-# Use scan and join to apply the regex and print the result
-puts input.scan(regex).join
+puts ARGV[0].scan(/hbt{2,5}n/).join
--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,9 +1,2 @@
 #!/usr/bin/env ruby
-
-input = ARGV[0]
-
-# Regular expression to match sequences of 'b' (1 or more times)
-regex = /b+/
-
-# Use scan and join to apply the regex and print the result
-puts input.scan(regex).join
+puts ARGV[0].scan(/hb?tn/).join
